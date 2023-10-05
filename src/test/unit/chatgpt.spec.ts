522ca4c83d252528f9590e8870f9053cb7b210eb
Phonarnun Tatiyamaneekul <phonarnun@hotmail.com>
2023-10-06 04:44:35 +0700

    fix(test): Add skip to test cases that require API

--- src/test/unit/chatgpt.spec.ts
+++ src/test/unit/chatgpt.spec.ts
@@ -1,9 +1,8 @@
 import { getResults } from "../../functions/ChatGPT";
 
-// // ! remove this test case because it requires API key
-// describe('ChatGPT function', () => {
-//     it('should return a string', async () => {
-//         console.log(await getResults("Hello"));
-//         expect( await getResults("Hello")).not.toBeNull();
-//     })
-// })
+describe.skip('ChatGPT function', () => {
+    it('should return a string', async () => {
+        console.log(await getResults("Hello"));
+        expect( await getResults("Hello")).not.toBeNull();
+    })
+})
