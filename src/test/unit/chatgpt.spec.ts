08bd7ddf211808761ecb9353256c74be93c3b919
Phonarnun Tatiyamaneekul <phonarnun@hotmail.com>
2023-10-06 04:23:06 +0700

    test: Add ChatGPT test case

---
+++ src/test/unit/chatgpt.spec.ts
@@ -0,0 +1,9 @@
+import { getResults } from "../../functions/ChatGPT";
+
+// // ! remove this test case because it requires API key
+// describe('ChatGPT function', () => {
+//     it('should return a string', async () => {
+//         console.log(await getResults("Hello"));
+//         expect( await getResults("Hello")).not.toBeNull();
+//     })
+// })
